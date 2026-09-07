b62a6bab8fb1ca68831896c8b3f71b1059d41237
hiro23900 <hiro23900@gmail.com>
2026-09-07 21:43:21 +0900

    mainメソッドを設定

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,12 +1,16 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-arys = []
-Dir.glob('*').each_slice(3) do |ary|
-  arys << if ary.length == 3
-            ary
-          else
-            ary.values_at(0..2)
-          end
+def main
+  arys = []
+  Dir.glob('*').each_slice(3) do |ary|
+    arys << if ary.length == 3
+              ary
+            else
+              ary.values_at(0..2)
+            end
+  end
+  pp arys
 end
-pp arys
+
+main
