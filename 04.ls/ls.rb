d48ea3476acdcff4a48f0268eb1277a0d9abc8e0
hiro23900 <hiro23900@gmail.com>
2026-09-08 00:15:59 +0900

    縦方向に整列

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,10 +1,19 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+COL_SPACE = 2
+
 def main
   table_col_count = 4
+
+  alined_files = []
+  files = Dir.glob('*')
+  files.each do |file|
+    alined_files << file.ljust(12 + COL_SPACE)
+  end
+
   arys = []
-  Dir.glob('*').each_slice(table_col_count) do |ary|
+  alined_files.each_slice(table_col_count) do |ary|
     arys << if ary.length == table_col_count
               ary
             else
